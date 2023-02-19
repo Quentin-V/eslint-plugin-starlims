dee696e0d2138d1d9c00bf838f39af700e1ab8a0
Quentin Vauthier <quentin.vauthier000@gmail.com>
2023-02-19 23:17:44 +0100

    added default configs

---
+++ src/configs.js
@@ -0,0 +1,22 @@
+module.exports = {
+    default: {
+        env: {
+            'browser': true,
+            'commonjs': true,
+            'es2021': true,
+            '@quintaaa/starlims/forms': true
+        },
+        extends: 'eslint:recommended',
+        parserOptions: {
+            'ecmaVersion': 'latest'
+        },
+        rules: {
+            'no-unused-vars': 'warn',
+            'no-undef': 'warn',
+            'prefer-const': 'warn',
+            'dot-notation': 'warn',
+            '@quintaaa/starlims/no-synchronous-requests': 'warn',
+            '@quintaaa/starlims/check-server-functions': 'warn',
+        }
+    },
+};
--- starlims.js
+++ starlims.js
@@ -1,8 +1,10 @@
-const processors = require("./src/processors");
-const formsenv = require("./src/envs/forms");
-const rules = require("./src/rules");
+const processors = require('./src/processors');
+const formsenv = require('./src/envs/forms');
+const rules = require('./src/rules');
+const configs = require('./src/configs');
 
 module.exports = {
+  configs: configs,
   processors: processors,
   environments: {
     forms: formsenv,
